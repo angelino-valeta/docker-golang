f5d6ca03e81c79a4814bef2fe983d7c5d5e1d67b
Angelino Valeta <angelinovaleta@gmail.com>
2023-10-15 13:10:16 +0100

    fix test

--- main_test.go
+++ main_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestingBasic(t *testing.T) {
-	ans := IntMin(2, 20)
+func TestIntBasic(t *testing.T) {
+	ans := IntMin(2, -2)
 	if ans != -2 {
 		t.Errorf("IntMin(2,-2) = %d; want -2", ans)
 	}
